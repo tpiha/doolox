3f68a622b5348c97eb199b53978516eb34a7ec7a
Tihomir Piha <tpiha@naklikaj.com>
2014-01-31 05:32:38 +0100

    fix

--- app/controllers/DooloxController.php
+++ app/controllers/DooloxController.php
@@ -532,7 +532,7 @@ class DooloxController extends BaseController {
         $privatekey = Config::get('doolox.fskey');
         $secdata = Request::header('security_data');
         $sechash = Request::header('security_hash');
-        Log::info('FastSpring ' . $_REQUEST['security_data']);
+        Log::info('FastSpring ' . var_dump($_REQUEST));
         // if (md5($secdata . $privatekey) == $sechash){
         //     die('received');
         // }
