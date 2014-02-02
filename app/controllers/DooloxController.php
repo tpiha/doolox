7ca02ec73d2933012391ebddd6b90f6ce1e23d27
Tihomir Piha <tpiha@naklikaj.com>
2014-02-02 22:10:02 +0100

    fix

--- app/controllers/DooloxController.php
+++ app/controllers/DooloxController.php
@@ -46,6 +46,9 @@ class DooloxController extends BaseController {
     {
         Validator::extend('wpurl', function($attribute, $value, $parameters)
         {
+            if (strpos($value, 'http://') !== 0 && strpos($value, 'https://') !== 0) {
+                $value = 'http://' . $value;
+            }
             if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/", $value)) {
                 return false;
             }
