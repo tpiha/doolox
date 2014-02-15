8adbcdd5c2737fe6b85a240aa24e729fde0b25ad
Tihomir Piha <tpiha@naklikaj.com>
2014-02-16 00:40:45 +0100

    fix

---
+++ lock_php.php
@@ -0,0 +1,9 @@
+<?php
+    $path = '/var/www/doolox/users/';
+    $cd = getcwd();
+    $user = str_replace($path, '', $cd);
+    $user = explode('/', $user);
+    $user = $user[0];
+    ini_set ('upload_tmp_dir', $path . '/' . $user . '/tmp/'); 
+    ini_set ('open_basedir', $path . '/' . $user . '/:/tmp/');
+?>
